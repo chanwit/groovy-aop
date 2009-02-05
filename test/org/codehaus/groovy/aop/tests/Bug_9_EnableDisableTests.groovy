638b59f5c6ba57b51a23d7665ad9a580eaccfc7c
chanwit <chanwit@lb.mshome.net>
2009-02-05 22:17:25 +0700

    rename to Bug_9

---
+++ test/org/codehaus/groovy/aop/tests/Bug_9_EnableDisableTests.groovy
@@ -0,0 +1,11 @@
+package org.codehaus.groovy.aop.tests
+
+import org.codehaus.groovy.aop.metaclass.AspectMetaClass
+import org.codehaus.groovy.aop.metaclass.AspectMetaclassCreationHandle
+class Bug_9_EnableDisableTests extends GroovyTestCase {
+
+    void testAutoEnable() {
+        assert AspectMetaclassCreationHandle.isEnabled() == true
+    }
+
+}
