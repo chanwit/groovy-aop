a93e01e1a9c9ee5a4a89953d92140cfc64a9969d
chanwit <chanwit@gmail.com>
2009-02-06 05:23:03 +0700

    changed because aspect MC is enabled

--- test/org/codehaus/groovy/aop/tests/AspectInstallationTests.groovy
+++ test/org/codehaus/groovy/aop/tests/AspectInstallationTests.groovy
@@ -1,18 +1,18 @@
 /**
  * Copyright 2007 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * @author Chanwit Kaewkasi
  *
  **/
@@ -25,50 +25,51 @@ import org.codehaus.groovy.aop.abstraction.advice.*
 import org.codehaus.groovy.aop.abstraction.pcd.*
 
 class AspectInstallationTests extends GroovyTestCase {
-	
+
 static code = '''
 package org.codehaus.groovy.aop.tests;
 
 class Test1Aspect {
 
-	static aspect = {
-		def pc = pcall('pattern1') & within('pattern2')
-		around(pc) { inv ->
-			inv.proceed(inv.args)
-		}
-	}
-	
+    static aspect = {
+        def pc = pcall('pattern1') & within('pattern2')
+        around(pc) { inv ->
+            inv.proceed(inv.args)
+        }
+    }
+
 }
 '''
-	void testInstall() {
-		AspectMetaClass.enableGlobally()	
-		def gcl = new GroovyClassLoader(this.getClass().getClassLoader())
-		assert gcl != null
-		def c = gcl.parseClass(code)
-		assert c != null
-		
-		Weaver.install(c)
-		def a = AspectRegistry.v().get(c)
-		assert a instanceof Aspect
-		def advice = a.advices[0] 
-		assert advice instanceof AroundAdvice
-		assert advice.pointcut !=null
-		assert advice.pointcut.root instanceof PCallPCD 
-		assert advice.pointcut.root.nextNode instanceof WithInPCD
-		Weaver.uninstall(c)
-		AspectMetaClass.disableGlobally()				
-	}
-	
-	void testUninstall() {
-		AspectMetaClass.enableGlobally()		
-		def gcl = new GroovyClassLoader(this.getClass().getClassLoader())
-		def c = gcl.parseClass(code)
-		
-		Weaver.install(c)
-		Weaver.uninstall(c)
-		def a = AspectRegistry.v().get(c)
-		assert a == null
-		AspectMetaClass.disableGlobally()		
-	}
+    void testInstall() {
+        // AspectMetaClass.enableGlobally()
+        def gcl = new GroovyClassLoader(this.getClass().getClassLoader())
+        assert gcl != null
+        def c = gcl.parseClass(code)
+        assert c != null
+
+        def a = Weaver.install(c)
+        def a1 = AspectRegistry.v().get(c)
+        assert a == a1
+        assert a instanceof Aspect
+        def advice = a.advices[0]
+        assert advice instanceof AroundAdvice
+        assert advice.pointcut !=null
+        assert advice.pointcut.root instanceof PCallPCD
+        assert advice.pointcut.root.nextNode instanceof WithInPCD
+        Weaver.uninstall(c)
+        // AspectMetaClass.disableGlobally()
+    }
+
+    void testUninstall() {
+        // AspectMetaClass.enableGlobally()
+        def gcl = new GroovyClassLoader(this.getClass().getClassLoader())
+        def c = gcl.parseClass(code)
+
+        Weaver.install(c)
+        Weaver.uninstall(c)
+        def a = AspectRegistry.v().get(c)
+        assert a == null
+        // AspectMetaClass.disableGlobally()
+    }
 
 }
