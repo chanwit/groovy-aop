47b1759fd8f9b2cdb8fc9662a405db9799a585d9
chanwit <chanwit@gmail.com>
2009-08-01 16:00:35 +0100

    updated script

--- src/main/java/org/codehaus/groovy/aop/metaclass/gen_aacs.groovy
+++ src/main/java/org/codehaus/groovy/aop/metaclass/gen_aacs.groovy
@@ -34,31 +34,35 @@ println header
 // each normal call will gen [],0,1,2,3,4
 //
 def normal = ['','Constructor','Current','Safe','Static']
+
+//
 // cv is call convention
+//
 normal.each { cv ->
   for(n in -1..4) {
     def args = []
     def params = []
     for(i in 0..n)  {
         // special case, for Object[]
-        if(i==-1) {
-            args << "Object[] arg1"
-            params << "arg1"
-        } else {        
-            if(cv == 'Current' && i == 0) 
+        if(i == -1) {
+            args << 'Object[] arg1'
+            params << 'arg1'
+        } else {
+            if(cv == 'Current' && i == 0)
                 args << "GroovyObject arg${i}"
-            else 
-            if(cv == 'Static' && i == 0) 
+            else if (cv == 'Static' && i == 0)
                 args << "Class arg${i}"
             else
                 args << "Object arg${i}"
+
             params << "arg${i}"
         }
     }
 def text = """
     @Override
     public Object call${cv}(${args.join(", ")}) throws Throwable {
-        return delegate.call${cv}(${params.join(", ")});
+        Object result = delegate.call${cv}(${params.join(", ")});
+        return result;
     }
 """
     print text
@@ -73,7 +77,8 @@ special_0.each { m ->
 def text = """
     @Override
     public Object ${m}(Object arg0) throws Throwable {
-        return delegate.${m}(arg0);
+        Object result = delegate.${m}(arg0);
+        return result;
     }
 """
     print text
