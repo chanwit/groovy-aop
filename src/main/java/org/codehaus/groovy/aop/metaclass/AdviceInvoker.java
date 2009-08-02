dfcc987afef40e058c85957d083c590ec215b5a1
chanwit <chanwit@gmail.com>
2009-08-02 09:51:44 +0100

    added adv-invoker generator

--- src/main/java/org/codehaus/groovy/aop/metaclass/AdviceInvoker.java
+++ src/main/java/org/codehaus/groovy/aop/metaclass/AdviceInvoker.java
@@ -15,8 +15,9 @@ public class AdviceInvoker {
         for(int i = 0; i < before.count; i++) {
             before[i].call(context);
         }
-        context.proceed = delegate;
-        around(context);
+        // context.proceed = delegate;
+        // around(context);
+        delegate.call();
         for(int i = 0; i < after.count; i++) {
             after[i].call(context);
         }        
---
+++ src/main/java/org/codehaus/groovy/aop/metaclass/gen_adv_inv.groovy
@@ -0,0 +1,100 @@
+#!/usr/bin/env groovy
+
+def header = '''
+package org.codehaus.groovy.aop.metaclass;
+
+import groovy.lang.GroovyObject;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.aspectj.weaver.tools.PointcutParser;
+import org.codehaus.groovy.runtime.callsite.CallSite;
+import org.codehaus.groovy.runtime.callsite.CallSiteArray;
+
+public class AdviceInvoker {
+    
+    private CallSite delegate;
+    private EffectiveAdvices ea;
+    
+    public AdviceInvoker (CallSite delegate, EffectiveAdvices effectiveAdviceCodes) {
+        this.delegate = delegate;
+        this.ea = effectiveAdviceCodes;
+    }
+'''
+
+println header
+
+//
+// each normal call will gen [],0,1,2,3,4
+//
+def normal = ['','Constructor','Current','Safe','Static']
+
+//
+// cv is call convention
+//
+normal.each { cv ->
+  for(n in -1..4) {
+    def args = []
+    def params = []
+    for(i in 0..n)  {
+        // special case, for Object[]
+        if(i == -1) {
+            args << 'Object[] arg1'
+            params << 'arg1'
+        } else {
+            if(cv == 'Current' && i == 0)
+                args << "GroovyObject arg${i}"
+            else if (cv == 'Static' && i == 0)
+                args << "Class arg${i}"
+            else
+                args << "Object arg${i}"
+
+            params << "arg${i}"
+        }
+    }
+def text = """
+    @Override
+    public Object call${cv}(${args.join(", ")}) throws Throwable {
+        for(int i = 0; i < before.count; i++) {
+            before[i].call(context);
+        }
+        Object result = delegate.call${cv}(${args.join(", ")});
+        for(int i = 0; i < after.count; i++) {
+            after[i].call(context);
+        }      
+        return result;  
+    }
+"""
+    print text
+  }
+}
+
+//
+// another set to gen *property with arg0
+//
+def special_0 = ['callGetProperty', 
+                 'callGetPropertySafe', 
+                 'callGroovyObjectGetProperty', 
+                 'callGroovyObjectGetPropertySafe']
+special_0.each { m ->
+def text = """
+    @Override
+    public Object ${m}(Object arg0) throws Throwable {
+        for(int i = 0; i < before.count; i++) {
+            before[i].call(context);
+        }
+        Object result = delegate.${m}(arg0);
+        for(int i = 0; i < after.count; i++) {
+            after[i].call(context);
+        }      
+        return result;
+    }
+"""
+    print text
+}
+
+def footer = '''
+}
+'''
+
+println footer
