ba0f456cba07d5f9f2021df84c45aad0869c2ce8
chanwit <chanwit@gmail.com>
2009-08-09 22:59:54 +0100

    refactored

--- src/test/groovy/org/codehaus/groovy/gjit/soot/factorial/FactorialAspect.groovy
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/factorial/FactorialAspect.groovy
@@ -2,13 +2,14 @@ package org.codehaus.groovy.gjit.soot.factorial;
 class FactorialAspect {
 
 	static aspect = {
-		def pc1 = pcall("Factorial.fac") & args(n)
+		def pc1 = pcall("${Factorial.class.name}.fac") & args(n)
 		typing(pc1) {
 			n >> int
 		}
 
-		def pc2 = ( pcall("Number.multiply") | pcall("Number.minus") ) &
-		            within("Factorial.fac")
+		def pc2 = ( pcall("${Integer.class.name}.multiply") |
+				    pcall("${Integer.class.name}.minus")    )  &
+		            within("${Factorial.class.name}.fac")
 		typing(pc2 & pthis(i) & args(j)) {
 			[i, j] >> int
 		}
--- src/test/groovy/org/codehaus/groovy/gjit/soot/fibbonacci/FibAspect.groovy
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/fibbonacci/FibAspect.groovy
@@ -2,9 +2,9 @@ package org.codehaus.groovy.gjit.soot.fibbonacci;
 class FibAspect {
 
 	static aspect = {
-		def pc1 = pcall("Fib.fib") & within("static Fib.main") & args(n)
-		def pc2 = pcall("Fib.fib") & args(n)
-		typing(pc1 | pc2) {
+		def Fib = "org.codehaus.groovy.gjit.soot.fibbonacci.Fib"
+		def pc = pcall("${Fib}.fib") & args(n)
+		typing(pc) {
 			n >> int
 		}
 	}
