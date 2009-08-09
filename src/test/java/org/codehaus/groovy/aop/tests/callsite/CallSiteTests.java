4a57bdfda012cb8821b3f8b82f585a3f352290f8
chanwit <chanwit@gmail.com>
2009-08-09 19:52:14 +0100

    fixed spacing

--- src/test/java/org/codehaus/groovy/aop/tests/callsite/CallSiteTests.java
+++ src/test/java/org/codehaus/groovy/aop/tests/callsite/CallSiteTests.java
@@ -1,10 +1,12 @@
 package org.codehaus.groovy.aop.tests.callsite;
 
 import junit.framework.*;
+
+import org.aspectj.lang.JoinPoint;
 import org.codehaus.groovy.aop.metaclass.*;
 
 public class CallSiteTests extends TestCase {
-    
+
     public void testSomething() throws Throwable {
         AspectAwareCallSite a = new AspectAwareCallSite(new MockCallSite());
         assertEquals("delegate.call(0);", a.call(0));
