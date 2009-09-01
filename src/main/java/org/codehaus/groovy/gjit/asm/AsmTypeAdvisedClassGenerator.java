cf6bd227cdcb01fdff982c9bb327b2358a9c7be0
chanwit <chanwit@gmail.com>
2009-09-01 11:19:23 +0100

    refactored type propagate to be another class

--- src/main/java/org/codehaus/groovy/gjit/asm/AsmTypeAdvisedClassGenerator.java
+++ src/main/java/org/codehaus/groovy/gjit/asm/AsmTypeAdvisedClassGenerator.java
@@ -5,20 +5,15 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.codehaus.groovy.gjit.asm.transformer.TypePropagateTransformer;
 import org.codehaus.groovy.runtime.callsite.CallSite;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
-import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.InsnList;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
-import org.objectweb.asm.tree.TypeInsnNode;
-import org.objectweb.asm.tree.VarInsnNode;
 
 public class AsmTypeAdvisedClassGenerator implements Opcodes {
 
@@ -121,7 +116,7 @@ public class AsmTypeAdvisedClassGenerator implements Opcodes {
         //
         // Perform a set of transformation
         //
-        typePropagate(targetMN);
+        new TypePropagateTransformer(advisedTypes, advisedReturnType).internalTransform(targetMN);
         optimiseBinaryOperators(targetMN);
 
         String newClassName = Type.getInternalName(callSite.getClass()) + "$x";
@@ -137,62 +132,6 @@ public class AsmTypeAdvisedClassGenerator implements Opcodes {
         return new Result(newClassName + "." + targetMN.name + methodDescriptor, cw.toByteArray());
     }
 
-    private void typePropagate(MethodNode m) {
-        final boolean staticMethod = (m.access & ACC_STATIC) != 0;
-        InsnList units = m.instructions;
-        AbstractInsnNode s = units.getFirst();
-        while(s != null) {
-            if(s.getOpcode() == ALOAD || s.getOpcode() == ASTORE) {
-                VarInsnNode v = (VarInsnNode)s;
-                Class<?> type;
-                if(staticMethod) {
-                    if(v.var >= advisedTypes.length) {
-                        s = s.getNext();
-                        continue;
-                    }
-                    type = advisedTypes[v.var];
-                }
-                else {
-                    if(v.var-1 >= advisedTypes.length) {
-                        s = s.getNext();
-                        continue;
-                    }
-                    type = advisedTypes[v.var - 1];
-                }
-                if(type != null && type.isPrimitive()) {
-                    int offset = 4;
-                    if(type == int.class)    offset = 4; else
-                    if(type == long.class)   offset = 3; else
-                    if(type == float.class)  offset = 2; else
-                    if(type == double.class) offset = 1;
-                    VarInsnNode newS = new VarInsnNode(v.getOpcode()-offset, v.var);
-                    units.set(s, newS);
-                    units.insert(newS, getBoxNode(type));
-                    s = newS.getNext().getNext();
-                    continue;
-                } else if(type != null) {
-                    throw new RuntimeException("NYI");
-                }
-            } else if (s.getOpcode() == ARETURN) {
-                if(advisedReturnType != null && advisedReturnType.isPrimitive()) {
-                    int offset = 4;
-                    if(advisedReturnType == int.class)    offset = 4; else
-                    if(advisedReturnType == long.class)   offset = 3; else
-                    if(advisedReturnType == float.class)  offset = 2; else
-                    if(advisedReturnType == double.class) offset = 1;
-                    InsnNode newS = new InsnNode(ARETURN - offset);
-                    units.set(s, newS);
-                    units.insertBefore(newS, getUnboxNodes(advisedReturnType));
-                    s = newS.getNext();
-                    continue;
-                } else if (advisedReturnType != null) {
-                    throw new RuntimeException("NYI");
-                }
-            }
-            s = s.getNext();
-        }
-    }
-
     /**
      * Check if there is a bytecode pattern that use typed local variable.
      * If so, tranform it with heuristic rules to be native xADD etc.
@@ -203,37 +142,4 @@ public class AsmTypeAdvisedClassGenerator implements Opcodes {
 
     }
 
-    private MethodInsnNode getBoxNode(Class<?> type) {
-        String name=null;
-        String desc=null;
-        if(type == int.class)     {name = "Integer";  desc = "I"; } else
-        if(type == long.class)    {name = "Long";     desc = "J"; } else
-        if(type == byte.class)    {name = "Byte";     desc = "B"; } else
-        if(type == boolean.class) {name = "Boolean";  desc = "Z"; } else
-        if(type == short.class)   {name = "Short";    desc = "S"; } else
-        if(type == double.class)  {name = "Double";   desc = "D"; } else
-        if(type == float.class)   {name = "Float";    desc = "F"; } else
-        if(type == char.class)    {name = "Character";desc = "C"; }
-        if(name == null) throw new RuntimeException("No box for " + type);
-        return new MethodInsnNode(INVOKESTATIC, "java/lang/" + name, "valueOf", "(" + desc + ")Ljava/lang/" + name + ";");
-    }
-
-    private InsnList getUnboxNodes(Class<?> type) {
-        InsnList result = new InsnList();
-        String name=null;
-        String shortName = type.getName();
-        String desc=null;
-        if(type == int.class)     {name = "Integer";  desc = "I"; } else
-        if(type == long.class)    {name = "Long";     desc = "J"; } else
-        if(type == byte.class)    {name = "Byte";     desc = "B"; } else
-        if(type == boolean.class) {name = "Boolean";  desc = "Z"; } else
-        if(type == short.class)   {name = "Short";    desc = "S"; } else
-        if(type == double.class)  {name = "Double";   desc = "D"; } else
-        if(type == float.class)   {name = "Float";    desc = "F"; } else
-        if(type == char.class)    {name = "Character";desc = "C"; }
-        if(name == null) throw new RuntimeException("No unbox for " + type);
-        result.add(new TypeInsnNode(CHECKCAST, "java/lang/" + name));
-        result.add(new MethodInsnNode(INVOKEVIRTUAL, "java/lang/" + name, shortName + "Value", "()" + desc));
-        return result;
-    }
 }
---
+++ src/main/java/org/codehaus/groovy/gjit/asm/transformer/TypePropagateTransformer.java
@@ -0,0 +1,113 @@
+package org.codehaus.groovy.gjit.asm.transformer;
+
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TypeInsnNode;
+import org.objectweb.asm.tree.VarInsnNode;
+
+public class TypePropagateTransformer implements Transformer, Opcodes {
+
+    final private Class<?>[] advisedTypes;
+    final private Class<?>   advisedReturnType;
+
+    public TypePropagateTransformer(Class<?>[] advisedTypes,
+                                    Class<?> advisedReturnType) {
+        this.advisedTypes = advisedTypes;
+        this.advisedReturnType = advisedReturnType;
+    }
+
+    @Override
+    public void internalTransform(MethodNode body) {
+        final boolean staticMethod = (body.access & ACC_STATIC) != 0;
+        InsnList units = body.instructions;
+        AbstractInsnNode s = units.getFirst();
+        while(s != null) {
+            if(s.getOpcode() == ALOAD || s.getOpcode() == ASTORE) {
+                VarInsnNode v = (VarInsnNode)s;
+                Class<?> type;
+                if(staticMethod) {
+                    if(v.var >= advisedTypes.length) {
+                        s = s.getNext();
+                        continue;
+                    }
+                    type = advisedTypes[v.var];
+                }
+                else {
+                    if(v.var-1 >= advisedTypes.length) {
+                        s = s.getNext();
+                        continue;
+                    }
+                    type = advisedTypes[v.var - 1];
+                }
+                if(type != null && type.isPrimitive()) {
+                    int offset = 4;
+                    if(type == int.class)    offset = 4; else
+                    if(type == long.class)   offset = 3; else
+                    if(type == float.class)  offset = 2; else
+                    if(type == double.class) offset = 1;
+                    VarInsnNode newS = new VarInsnNode(v.getOpcode()-offset, v.var);
+                    units.set(s, newS);
+                    units.insert(newS, getBoxNode(type));
+                    s = newS.getNext().getNext();
+                    continue;
+                } else if(type != null) {
+                    throw new RuntimeException("NYI");
+                }
+            } else if (s.getOpcode() == ARETURN) {
+                if(advisedReturnType != null && advisedReturnType.isPrimitive()) {
+                    int offset = 4;
+                    if(advisedReturnType == int.class)    offset = 4; else
+                    if(advisedReturnType == long.class)   offset = 3; else
+                    if(advisedReturnType == float.class)  offset = 2; else
+                    if(advisedReturnType == double.class) offset = 1;
+                    InsnNode newS = new InsnNode(ARETURN - offset);
+                    units.set(s, newS);
+                    units.insertBefore(newS, getUnboxNodes(advisedReturnType));
+                    s = newS.getNext();
+                    continue;
+                } else if (advisedReturnType != null) {
+                    throw new RuntimeException("NYI");
+                }
+            }
+            s = s.getNext();
+        }
+    }
+
+    private MethodInsnNode getBoxNode(Class<?> type) {
+        String name=null;
+        String desc=null;
+        if(type == int.class)     {name = "Integer";  desc = "I"; } else
+        if(type == long.class)    {name = "Long";     desc = "J"; } else
+        if(type == byte.class)    {name = "Byte";     desc = "B"; } else
+        if(type == boolean.class) {name = "Boolean";  desc = "Z"; } else
+        if(type == short.class)   {name = "Short";    desc = "S"; } else
+        if(type == double.class)  {name = "Double";   desc = "D"; } else
+        if(type == float.class)   {name = "Float";    desc = "F"; } else
+        if(type == char.class)    {name = "Character";desc = "C"; }
+        if(name == null) throw new RuntimeException("No box for " + type);
+        return new MethodInsnNode(INVOKESTATIC, "java/lang/" + name, "valueOf", "(" + desc + ")Ljava/lang/" + name + ";");
+    }
+
+    private InsnList getUnboxNodes(Class<?> type) {
+        InsnList result = new InsnList();
+        String name=null;
+        String shortName = type.getName();
+        String desc=null;
+        if(type == int.class)     {name = "Integer";  desc = "I"; } else
+        if(type == long.class)    {name = "Long";     desc = "J"; } else
+        if(type == byte.class)    {name = "Byte";     desc = "B"; } else
+        if(type == boolean.class) {name = "Boolean";  desc = "Z"; } else
+        if(type == short.class)   {name = "Short";    desc = "S"; } else
+        if(type == double.class)  {name = "Double";   desc = "D"; } else
+        if(type == float.class)   {name = "Float";    desc = "F"; } else
+        if(type == char.class)    {name = "Character";desc = "C"; }
+        if(name == null) throw new RuntimeException("No unbox for " + type);
+        result.add(new TypeInsnNode(CHECKCAST, "java/lang/" + name));
+        result.add(new MethodInsnNode(INVOKEVIRTUAL, "java/lang/" + name, shortName + "Value", "()" + desc));
+        return result;
+    }
+}
