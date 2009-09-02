e2db87883db16a32c7634d771e19e60dfa32a203
chanwit <chanwit@gmail.com>
2009-09-03 00:58:03 +0100

    implemented fast call site name collector

---
+++ src/main/java/org/codehaus/groovy/gjit/asm/transformer/CallSiteNameCollector.java
@@ -0,0 +1,59 @@
+package org.codehaus.groovy.gjit.asm.transformer;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.codehaus.groovy.gjit.asm.CallSiteNameHolder;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.LdcInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+public class CallSiteNameCollector implements Transformer, Opcodes {
+
+//	  // access flags 4106
+//	  private static $createCallSiteArray()Lorg/codehaus/groovy/runtime/callsite/CallSiteArray;
+//	    NEW org/codehaus/groovy/runtime/callsite/CallSiteArray
+//	    DUP
+//	    GETSTATIC org/codehaus/groovy/gjit/soot/fibbonacci/Fib.$ownClass : Ljava/lang/Class;
+//	    LDC 7
+//	    ANEWARRAY java/lang/String
+//	    DUP
+//	    LDC 0
+//	    LDC "plus"
+//	    AASTORE
+//	    DUP
+//      ...
+//	    AASTORE
+//	    INVOKESPECIAL org/codehaus/groovy/runtime/callsite/CallSiteArray.<init>(Ljava/lang/Class;[Ljava/lang/String;)V
+//	    ARETURN
+//	    MAXSTACK = 7
+//	    MAXLOCALS = 0
+
+    @Override
+    public void internalTransform(MethodNode body, Map<String, Object> options) {
+        if(body.name.equals("$createCallSiteArray") == false) return;
+        if(body.desc.equals("()Lorg/codehaus/groovy/runtime/callsite/CallSiteArray;")==false) return;
+
+        InsnList units = body.instructions;
+        AbstractInsnNode s = units.getFirst();
+        ArrayList<String> names = new ArrayList<String>();
+        String owner = null;
+        while(s != null) {
+            if(s.getOpcode() == GETSTATIC && ((FieldInsnNode)s).name.equals("$ownClass")) {
+                FieldInsnNode f = (FieldInsnNode)s;
+                owner = f.owner;
+            } else if(s.getOpcode() == LDC) {
+                 LdcInsnNode ldc = ((LdcInsnNode)s);
+                 if(ldc.cst instanceof String) {
+                     names.add((String) ldc.cst);
+                 }
+            }
+            s = s.getNext();
+        }
+        CallSiteNameHolder.v().put(owner, names.toArray(new String[names.size()]));
+    }
+
+}
