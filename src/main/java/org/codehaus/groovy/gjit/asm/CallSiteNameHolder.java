4a91294e9eaad5514040d2bc2db3e44a02bdd923
chanwit <chanwit@gmail.com>
2009-09-03 00:12:12 +0100

    added call site name holder for asm

---
+++ src/main/java/org/codehaus/groovy/gjit/asm/CallSiteNameHolder.java
@@ -0,0 +1,20 @@
+package org.codehaus.groovy.gjit.asm;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CallSiteNameHolder extends ConcurrentHashMap<String, String[]> {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -6780780650503305589L;
+    private static CallSiteNameHolder instance;
+
+    public static ConcurrentHashMap<String, String[]> v() {
+        if(instance == null)
+            instance = new CallSiteNameHolder();
+
+        return instance;
+    }
+
+}
