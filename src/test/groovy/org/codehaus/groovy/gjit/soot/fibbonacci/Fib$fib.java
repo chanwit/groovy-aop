d4cfd120507dab7c2a23c3c8c1f9b99f99f9f11b
chanwit <chanwit@gmail.com>
2009-09-04 21:43:28 +0100

    improved a better mock call site object

--- src/test/groovy/org/codehaus/groovy/gjit/soot/fibbonacci/Fib$fib.java
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/fibbonacci/Fib$fib.java
@@ -5,8 +5,18 @@ import org.codehaus.groovy.runtime.callsite.CallSiteArray;
 
 class Fib$fib extends AbstractCallSite {
 
+    private static final String[] NAMES = new String[]{
+        "fib",
+        "fib",
+        "fib",
+        "fib",
+        "fib",
+        "fib",
+        "fib"
+    };
+
     public Fib$fib() {
-        super(new CallSiteArray(Fib.class, new String[]{"a"}),0, "a");
+        super(new CallSiteArray(Fib.class, NAMES),6, "fib");
     }
 
 }
