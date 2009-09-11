853f64aba627533aaf762300c38c113df6c2390f
chanwit <chanwit@gmail.com>
2009-09-11 20:55:41 +0100

    added a mock call site for plus(I,I)

---
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/fibbonacci/NumberNumberPlus$IntegerInteger.java
@@ -0,0 +1,12 @@
+package org.codehaus.groovy.gjit.soot.fibbonacci;
+
+import org.codehaus.groovy.runtime.callsite.AbstractCallSite;
+import org.codehaus.groovy.runtime.callsite.CallSiteArray;
+
+public class NumberNumberPlus$IntegerInteger extends AbstractCallSite {
+
+    public NumberNumberPlus$IntegerInteger(Class<?> cls, String[] names, int i) {
+        super(new CallSiteArray(cls, names),i, names[i]);
+    }
+
+}
