f87d5f736d3cb8807c0186292f9c20fcc8285b6f
chanwit <chanwit@gmail.com>
2009-09-13 22:18:00 +0100

    updated tests

--- src/test/groovy/org/codehaus/groovy/gjit/asm/fibbonacci/FibTest.groovy
+++ src/test/groovy/org/codehaus/groovy/gjit/asm/fibbonacci/FibTest.groovy
@@ -4,7 +4,18 @@ import groovy.lang.ExpandoMetaClass;
 import groovy.util.GroovyTestCase
 class FibTest extends GroovyTestCase {
 
-    void testRunFib() {
+//    void testRunFib_01_Unadvised() {
+//        ExpandoMetaClass.enableGlobally()
+//        Caller.realTest(5)
+//        Caller.realTest(10)
+//        Caller.realTest(15)
+//        Caller.realTest(20)
+//        Caller.realTest(25)
+//        Caller.realTest(30)
+//        Caller.realTest(35)
+//    }
+
+    void testRunFib_02_Advised() {
         ExpandoMetaClass.enableGlobally()
         def aspect = weave(FibAspect)
         assert aspect != null
