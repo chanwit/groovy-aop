9b066e8606b68787f288f63df4df42698cf3229c
kaewkasc <kaewkasc@rpc114.cs.man.ac.uk>
2009-09-23 17:32:56 +0100

    started working on heapsort an arrayaccess tests

---
+++ src/test/groovy/org/codehaus/groovy/geelab/HeapSort.groovy
@@ -0,0 +1,61 @@
+package org.codehaus.groovy.geelab;
+
+public class HeapSort {
+
+	int dataSize;
+	double[] pArray;
+
+	public HeapSort() {
+		dataSize = 5000000; // 5 million doubles
+		randomizer.resetRandomizer();
+
+		// create test data array
+
+		pArray = new double[dataSize];
+		for (int i=0; i<dataSize; ++i)
+			pArray[i] = randomizer.getRandomValue(1);
+	}
+
+	protected void heapsort(int n, double[] ra) {
+		int l, ir;
+		double rra;
+
+		l = (n >> 1) + 1;
+		ir = n;
+		for (;;)
+		{
+			if (l > 1)
+				rra = ra[--l];
+			else
+			{
+				rra = ra[ir];
+				ra[ir] = ra[1];
+				if (--ir == 1)
+				{
+					ra[1] = rra;
+					return;
+				}
+			}
+
+			int i = l;
+			int j = l << 1;
+
+			while (j <= ir)
+			{
+
+				if (j < ir && ra[j] < ra[j+1])
+					++j;
+
+				if (rra < ra[j])
+				{
+					ra[i] = ra[j];
+					j += (i = j);
+				}
+				else
+					j = ir + 1;
+			}
+
+			ra[i] = rra;
+		}
+	}
+}
