389652a9fc6b650bc89cd2cdb5a6056e67318dfd
Chanwit Kaewkasi <chanwit@gmail.com>
2009-09-29 10:03:53 +0100

    heapsort test

--- src/test/groovy/org/codehaus/groovy/gjit/asm/heapsort/HeapSortTest.groovy
+++ src/test/groovy/org/codehaus/groovy/gjit/asm/heapsort/HeapSortTest.groovy
@@ -3,6 +3,8 @@ package org.codehaus.groovy.gjit.asm.heapsort;
 import groovy.util.GroovyTestCase
 import org.codehaus.groovy.gjit.asm.*
 import org.codehaus.groovy.gjit.asm.transformer.*
+import org.objectweb.asm.*
+import org.objectweb.asm.tree.*
 
 public class HeapSortTest extends GroovyTestCase {
 
@@ -11,7 +13,23 @@ public class HeapSortTest extends GroovyTestCase {
 
 		def sender = HeapSort.class
 		def sco  = new AsmSingleClassOptimizer()
-		def aatf = new AspectAwareTransformer()
+		def aatf = new AspectAwareTransformer(
+			advisedTypes:[int, double[]] as Class[],
+			advisedReturnType: void,
+			callSite: new HeapSort$heapsort(),
+			withInMethodName: "main"
+		)
+		sco.transformers = [
+		  DeConstantTransformer.class,
+		  aatf,
+		  AutoBoxEliminatorTransformer.class
+		]
+		byte[] bytes = sco.optimize(sender.name)
+		def cr = new ClassReader(bytes)
+		def cn = new ClassNode()
+		cr.accept cn, 0
+		def main = cn.methods.find { it.name == "main" }
+		assert main.name == "main"
 	}
 
 }
