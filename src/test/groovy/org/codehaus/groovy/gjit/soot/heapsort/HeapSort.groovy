414fa70898b317358c646eb19cc2dd2d3616d1dd
chanwit <chanwit@gmail.com>
2009-09-30 18:30:29 +0700

    reverted back to the full heapsort

--- src/test/groovy/org/codehaus/groovy/gjit/soot/heapsort/HeapSort.groovy
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/heapsort/HeapSort.groovy
@@ -17,31 +17,28 @@ class HeapSort {
         l = (n >> 1) + 1
         ir = n
         while (true) {
-            rra = ra[l]
-            ra[10] = rra
-            return
-//            if (l > 1) {
-//                rra = ra[--l]
-//            } else {
-//                rra = ra[ir]
-//                ra[ir] = ra[1]
-//                if (--ir == 1) {
-//                    ra[1] = rra
-//                    return
-//                }
-//            }
-//            i = l
-//            j = l << 1
-//            while (j <= ir) {
-//                if (j < ir && ra[j] < ra[j+1]) { ++j }
-//                if (rra < ra[j]) {
-//                    ra[i] = ra[j]
-//                    j += (i = j)
-//                } else {
-//                    j = ir + 1
-//                }
-//            }
-//            ra[i] = rra
+            if (l > 1) {
+                rra = ra[--l]
+            } else {
+                rra = ra[ir]
+                ra[ir] = ra[1]
+                if (--ir == 1) {
+                    ra[1] = rra
+                    return
+                }
+            }
+            i = l
+            j = l << 1
+            while (j <= ir) {
+                if (j < ir && ra[j] < ra[j+1]) { ++j }
+                if (rra < ra[j]) {
+                    ra[i] = ra[j]
+                    j += (i = j)
+                } else {
+                    j = ir + 1
+                }
+            }
+            ra[i] = rra
         }
     }
 
