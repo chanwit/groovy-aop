ee2a002a440d62d4260b0c987237dc9bf44ec532
Chanwit Kaewkasi <chanwit@gmail.com>
2009-10-01 12:04:16 +0100

    added b0

---
+++ src/test/groovy/org/codehaus/groovy/gjit/asm/breakdown/HeapSort_b0_OptimisationTests.groovy
@@ -0,0 +1,49 @@
+package org.codehaus.groovy.gjit.asm.breakdown
+
+import org.objectweb.asm.*
+import org.objectweb.asm.tree.*
+import org.objectweb.asm.util.*
+
+import groovy.util.GroovyTestCase
+import org.codehaus.groovy.gjit.soot.heapsort.b0.*
+import org.codehaus.groovy.gjit.asm.*
+import org.codehaus.groovy.gjit.asm.transformer.*
+
+public class HeapSort_b0_OptimisationTests extends GroovyTestCase implements Opcodes {
+
+    static HEAPSORT_X = "org/codehaus/groovy/gjit/soot/heapsort/b1/HeapSort_heapsort_x"
+
+    void testOptimiseOnHeapSort_b1() {
+        InsnListHelper.install()
+
+        def sender = HeapSort.class
+        def sco  = new AsmSingleClassOptimizer()
+        def aatf = new AspectAwareTransformer(
+            advisedTypes:[int, double[]] as Class[],
+            advisedReturnType: void,
+            callSite: new HeapSort$heapsort(18),
+            withInMethodName: "main"
+        )
+        sco.transformers = [
+          DeConstantTransformer.class,
+          aatf,
+          AutoBoxEliminatorTransformer.class
+        ]
+        byte[] bytes = sco.optimize(sender.name)
+        def cr = new ClassReader(bytes)
+        def cn = new ClassNode()
+        cr.accept cn, 0
+        def main = cn.methods.find { it.name == "main" }
+        assert main.name == "main"
+        // CheckClassAdapter.verify(cr, false, new PrintWriter(System.out))
+
+        def heapsort_x_body = ClassBodyCache.v().get(HEAPSORT_X)
+        assert heapsort_x_body != null
+
+        cr = new ClassReader(heapsort_x_body)
+        CheckClassAdapter.verify(cr, false, new PrintWriter(System.out))
+        def tcv = new TraceClassVisitor(new PrintWriter(System.out))
+        cr.accept tcv, 0
+    }
+
+}
---
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/heapsort/b0/HeapSort.groovy
@@ -0,0 +1,40 @@
+package org.codehaus.groovy.gjit.soot.heapsort.b0
+
+import org.codehaus.groovy.gjit.benchmarks.Random
+
+/*
+    The Computer Language Shootout
+    http://shootout.alioth.debian.org/
+
+    contributed by Jochen Hinrichsen
+*/
+class HeapSort {
+
+    static heapsort(n, ra) {
+        def l, j, ir, i
+        def rra
+
+        l = (n >> 1) + 1
+        ir = n
+        while (true) {
+        	ra[ir] = ra[1]
+        }
+    }
+
+    static void main(args) {
+        def N = (args.length == 0) ? 1000 : args[0].toInteger()
+        def nf = java.text.NumberFormat.getInstance()
+        nf.setMaximumFractionDigits(10)
+        nf.setMinimumFractionDigits(10)
+        nf.setGroupingUsed(false)
+        double[] ary = (double[]) java.lang.reflect.Array.newInstance(double.class, N)
+        def rndnum = new Random(1729)
+        for (i in 0..<N) {
+            ary[i] = rndnum.nextDouble()
+        }
+        heapsort(N-1, ary)
+        println ary[0]
+        println ary[N-2]
+        println ary[N-1]
+    }
+}
