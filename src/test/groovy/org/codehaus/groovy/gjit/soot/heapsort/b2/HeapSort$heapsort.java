9dabc4fe432d3ef5291596a3841c1550d9a75fdb
Chanwit Kaewkasi <chanwit@gmail.com>
2009-10-01 11:16:46 +0100

    added break down tests

---
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/heapsort/b2/HeapSort$heapsort.java
@@ -0,0 +1,23 @@
+package org.codehaus.groovy.gjit.soot.heapsort.b2;
+
+import org.codehaus.groovy.runtime.callsite.AbstractCallSite;
+import org.codehaus.groovy.runtime.callsite.CallSiteArray;
+
+class HeapSort$heapsort extends AbstractCallSite {
+
+    private static final String[] NAMES = new String[]{
+        "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort",
+        "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort",
+        "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort",
+        "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort", "heapsort"
+    };
+
+    public HeapSort$heapsort() {
+        super(new CallSiteArray(HeapSort.class, NAMES), 37, "heapsort");
+    }
+
+    public HeapSort$heapsort(int index) {
+        super(new CallSiteArray(HeapSort.class, NAMES), index, "heapsort");
+    }
+
+}
