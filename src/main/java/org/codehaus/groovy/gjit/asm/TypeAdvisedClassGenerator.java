feef9abb6334abdf6ccc625960c5c16c57035bde
chanwit <chanwit@gmail.com>
2009-10-03 01:05:39 +0700

    added another transformer

--- src/main/java/org/codehaus/groovy/gjit/asm/TypeAdvisedClassGenerator.java
+++ src/main/java/org/codehaus/groovy/gjit/asm/TypeAdvisedClassGenerator.java
@@ -43,6 +43,7 @@ public class TypeAdvisedClassGenerator implements Opcodes {
             new InferLocalsTransformer(),
             new NullInitToZeroTransformer(),
             new AutoBoxEliminatorTransformer(),
+            new XLoadBoxPopEliminatorTransformer(),
         };
     }
 
---
+++ src/main/java/org/codehaus/groovy/gjit/asm/transformer/XLoadBoxPopEliminatorTransformer.java
@@ -0,0 +1,53 @@
+package org.codehaus.groovy.gjit.asm.transformer;
+
+import java.util.Map;
+
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TypeInsnNode;
+
+public class XLoadBoxPopEliminatorTransformer implements Transformer, Opcodes {
+
+//  Remove this pattern:
+//    DLOAD 9
+//    INVOKESTATIC java/lang/Double.valueOf (D)Ljava/lang/Double;
+//    POP
+//  To:
+
+    @Override
+    public void internalTransform(MethodNode body, Map<String, Object> options) {
+        InsnList units = body.instructions;
+        AbstractInsnNode s = units.getFirst();
+        while(s != null) {
+            if(s.getOpcode() !=  POP)          { s = s.getNext(); continue; }
+            AbstractInsnNode p0 = s.getPrevious();
+            if(p0.getOpcode() != INVOKESTATIC) { s = s.getNext(); continue; }
+            AbstractInsnNode p1 = p0.getPrevious();
+            if(!(p1.getOpcode() >= ILOAD && p1.getOpcode() <= DLOAD))
+                 { s = s.getNext(); continue; }
+            
+            MethodInsnNode m0 = (MethodInsnNode)p0;
+            if(m0.name.equals("valueOf") == false) { s = s.getNext(); continue; }
+            
+            char type  = m0.desc.charAt(1);
+            int opcode = o1.getOpcode();
+            
+            if(type == 'I' && opcode == ILOAD ||
+               type == 'L' && opcode == LLOAD ||
+               type == 'F' && opcode == FLOAD ||
+               type == 'D' && opcode == DLOAD) {
+                s = s.getNext();
+                units.remove(s.getPrevious());
+                units.remove(p0);
+                units.remove(p1);
+                continue;
+            }
+
+            s = s.getNext();            
+        }
+    }
+}
