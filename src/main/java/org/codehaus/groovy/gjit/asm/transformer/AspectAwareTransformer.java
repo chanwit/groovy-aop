f7e02e06892d0dfbda6d1d60bc2718e6f93b39db
chanwit <chanwit@gmail.com>
2009-10-02 14:51:07 +0700

    forgot utils.

--- src/main/java/org/codehaus/groovy/gjit/asm/transformer/AspectAwareTransformer.java
+++ src/main/java/org/codehaus/groovy/gjit/asm/transformer/AspectAwareTransformer.java
@@ -110,14 +110,14 @@ public class AspectAwareTransformer implements Transformer, Opcodes {
                 VarInsnNode v = (VarInsnNode)array[1];
                 AbstractInsnNode newS = new VarInsnNode(ILOAD, v.var);
                 units.set(array[1], newS);
-                units.insert(newS, getBoxNode(int.class));
+                units.insert(newS, Utils.getBoxNode(int.class));
                 var1 = v.var;
             }
             if(array[2].getOpcode() == ALOAD) {
                 VarInsnNode v = (VarInsnNode)array[2];
                 AbstractInsnNode newS = new VarInsnNode(ILOAD, v.var);
                 units.set(array[2], newS);
-                units.insert(newS, getBoxNode(int.class));
+                units.insert(newS, Utils.getBoxNode(int.class));
                 var2 = v.var;
             }
             if(var1 != -1)
