d69ecd8f0d015ce2379ed5c6fdf74e3f139cf763
Chanwit Kaewkasi <chanwit@gmail.com>
2009-10-03 12:21:44 +0100

    ported FFT

--- src/test/groovy/org/codehaus/groovy/gjit/soot/fft/FFT.groovy
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/fft/FFT.groovy
@@ -1,5 +1,7 @@
 package org.codehaus.groovy.gjit.soot.fft
 
+import org.codehaus.groovy.gjit.benchmarks.Random
+
 /**
  * Computes FFT's of complex, double precision data where n is an integer power
  * of 2. This appears to be slower than the Radix2 method, but the code is
@@ -22,83 +24,79 @@ class FFT {
     }
 
     /** Compute Fast Fourier Transform of (complex) data, in place. */
-    static transform(dat) {
+    static transform(double[] data) {
         transform_internal(data, -1)
     }
 
     /** Compute Inverse Fast Fourier Transform of (complex) data, in place. */
-    static inverse(data) {
+    static inverse(double[] data) {
         transform_internal(data, +1)
         // Normalize
-        def nd = data.length
-        def n = nd / 2
-        def norm = 1 / n
+        int nd = data.length
+        int n = nd / 2
+        double norm = 1 / ((double) n)
         for (int i = 0; i < nd; i++)
-            data[i] *= norm
+            data[i] *= norm;
     }
 
     /**
      * Accuracy check on FFT of data. Make a copy of data, Compute the FFT, then
      * the inverse and compare to the original. Returns the rms difference.
      */
-    static test(data) {
-        def nd = data.length
+    static test(double[] data) {
+        int nd = data.length
         // Make duplicate for comparison
-        def copy = new double[nd]
+        double[] copy = new double[nd]
         System.arraycopy(data, 0, copy, 0, nd)
         // Transform & invert
         transform(data)
         inverse(data)
         // Compute RMS difference.
-        def diff = 0.0
+        double diff = 0.0
         for (int i = 0; i < nd; i++) {
-            def d = data[i] - copy[i]
+            double d = data[i] - copy[i]
             diff += d * d
         }
         return Math.sqrt(diff / nd)
     }
 
     /** Make a random array of n (complex) elements. */
-    public static double[] makeRandom(int n) {
+    static makeRandom(int n) {
         int nd = 2 * n
-        double data[] = new double[nd]
-        Random random = new Random(1729)
-        for (int i = 0; i < nd; i++)
-            data[i] = random.nextDouble()
+        double[] data = new double[nd]
+        def random = new Random(1729)
+        nd.times { i -> data[i] = random.nextDouble() }
         return data
     }
 
     /** Simple Test routine. */
-    public static void main(String args[]) {
+    static void main(args) {
         if (args.length == 0) {
             int n = 1024
-            System.out.println("n=" + n + " => RMS Error="
-                    + test(makeRandom(n)))
+            println "n=${n} => RMS Error=${test(makeRandom(n))}"
         }
         for (int i = 0; i < args.length; i++) {
             int n = Integer.parseInt(args[i])
-            System.out.println("n=" + n + " => RMS Error="
-                    + test(makeRandom(n)))
+            println "n=${n} => RMS Error=${test(makeRandom(n))}"
         }
     }
 
     /* ______________________________________________________________________ */
 
-    protected static int log2(int n) {
+    static log2(int n) {
         int log = 0
-        for (int k = 1; k < n; k *= 2, log++)
-            
+        for (int k = 1; k < n; k *= 2) log++;
         if (n != (1 << log))
-            throw new Error("FFT: Data length is not a power of 2!: " + n)
+            throw new Error("FFT: Data length is not a power of 2!: $n")
         return log
     }
 
-    protected static void transform_internal(double data[], int direction) {
+    static transform_internal(double[] data, int direction) {
         if (data.length == 0)
             return
         int n = data.length / 2
         if (n == 1)
-            return; // Identity operation!
+            return // Identity operation!
         int logn = log2(n)
 
         /* bit reverse the input data for decimation in time algorithm */
@@ -106,7 +104,8 @@ class FFT {
 
         /* apply fft recursion */
         /* this loop executed log2(N) times */
-        for (int bit = 0, dual = 1; bit < logn; bit++, dual *= 2) {
+        int dual = 1
+        for (int bit = 0; bit < logn; bit++) {
             double w_real = 1.0
             double w_imag = 0.0
 
@@ -120,24 +119,23 @@ class FFT {
                 int i = 2 * b
                 int j = 2 * (b + dual)
 
-                double wd_real = data[j]
+                double wd_real = data[j    ]
                 double wd_imag = data[j + 1]
 
-                data[j] = data[i] - wd_real
-                data[j + 1] = data[i + 1] - wd_imag
-                data[i] += wd_real
+                data[j    ] =  data[i    ] - wd_real
+                data[j + 1] =  data[i + 1] - wd_imag
+                data[i    ] += wd_real
                 data[i + 1] += wd_imag
             }
 
             /* a = 1 .. (dual-1) */
-            for (int a = 1; a < dual; a++) {
+            for (a in 1..<dual) {
                 /* trignometric recurrence for w-> exp(i theta) w */
-                {
-                    double tmp_real = w_real - s * w_imag - s2 * w_real
-                    double tmp_imag = w_imag + s * w_real - s2 * w_imag
-                    w_real = tmp_real
-                    w_imag = tmp_imag
-                }
+                double tmp_real = w_real - s * w_imag - s2 * w_real
+                double tmp_imag = w_imag + s * w_real - s2 * w_imag
+                w_real = tmp_real
+                w_imag = tmp_imag
+
                 for (int b = 0; b < n; b += 2 * dual) {
                     int i = 2 * (b + a)
                     int j = 2 * (b + a + dual)
@@ -148,16 +146,17 @@ class FFT {
                     double wd_real = w_real * z1_real - w_imag * z1_imag
                     double wd_imag = w_real * z1_imag + w_imag * z1_real
 
-                    data[j] = data[i] - wd_real
+                    data[j    ] = data[i] - wd_real
                     data[j + 1] = data[i + 1] - wd_imag
-                    data[i] += wd_real
+                    data[i    ] += wd_real
                     data[i + 1] += wd_imag
                 }
             }
+            dual *= 2
         }
     }
 
-    protected static void bitreverse(double data[]) {
+    static bitreverse(double[] data) {
         /* This is the Goldrader bit-reversal algorithm */
         int n = data.length / 2
         int nm1 = n - 1
@@ -165,14 +164,11 @@ class FFT {
         int j = 0
         for (; i < nm1; i++) {
 
-            // int ii = 2*i
-            int ii = i << 1
+            int ii = 2*i
 
-            // int jj = 2*j
-            int jj = j << 1
+            int jj = 2*j
 
-            // int k = n / 2 
-            int k = n >> 1
+            int k = n / 2
 
             if (i < j) {
                 double tmp_real = data[ii]
@@ -184,11 +180,9 @@ class FFT {
             }
 
             while (k <= j) {
-                // j = j - k 
-                j -= k
+                j = j - k
 
-                // k = k / 2 
-                k >>= 1
+                k = k / 2
             }
             j += k
         }
