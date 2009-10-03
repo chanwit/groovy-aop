38a4c9e7f1ceec9fba506870c65be023d23f57e5
chanwit <chanwit@gmail.com>
2009-10-04 00:37:57 +0700

    new mock class and call site

---
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/fft/FFT_transform_x$transformInternal.java
@@ -0,0 +1,34 @@
+package org.codehaus.groovy.gjit.soot.fft;
+
+import org.codehaus.groovy.runtime.callsite.AbstractCallSite;
+import org.codehaus.groovy.runtime.callsite.CallSiteArray;
+
+public class FFT_transform_x$transformInternal extends AbstractCallSite {
+
+    private static final String[] NAMES = new String[] {
+        "plus", "multiply", "minus", "multiply", "log2", "multiply", "plus", "transformInternal", "transformInternal", "length",
+        "div", "div", "putAt", "multiply", "getAt", "next", "length", "arraycopy", "transform", "inverse",
+        "iterator", "minus", "getAt", "getAt", "plus", "multiply", "sqrt", "div", "multiply", "<$constructor$>",
+        "iterator", "putAt", "nextDouble", "length", "println", "test", "makeRandom", "length", "parseInt", "getAt",
+        "println", "test", "makeRandom", "next", "next", "multiply", "leftShift", "<$constructor$>", "length", "div",
+        "length", "log2", "bitreverse", "div", "multiply", "multiply", "PI", "multiply", "sin", "sin",
+        "div", "multiply", "multiply", "multiply", "multiply", "plus", "getAt", "getAt", "plus", "putAt",
+        "minus", "getAt", "putAt", "plus", "minus", "getAt", "plus", "putAt", "plus", "getAt",
+        "putAt", "plus", "getAt", "plus", "plus", "multiply", "iterator", "minus", "minus", "multiply",
+        "multiply", "minus", "plus", "multiply", "multiply", "multiply", "plus", "multiply", "plus", "plus",
+        "getAt", "getAt", "plus", "minus", "multiply", "multiply", "plus", "multiply", "multiply", "putAt",
+        "minus", "getAt", "putAt", "plus", "minus", "getAt", "plus", "putAt", "plus", "getAt",
+        "putAt", "plus", "getAt", "plus", "plus", "multiply", "multiply", "next", "div", "length",
+        "minus", "multiply", "multiply", "div", "getAt", "getAt", "plus", "putAt", "getAt", "putAt",
+        "plus", "getAt", "plus", "putAt", "putAt", "plus", "minus", "div", "plus", "next"
+    };
+
+    public FFT_transform_x$transformInternal() {
+        super(new CallSiteArray(FFT_transform_x.class, NAMES), 7, NAMES[7]);
+    }
+
+    public FFT_transform_x$transformInternal(int index) {
+        super(new CallSiteArray(FFT_transform_x.class, NAMES), index, NAMES[index]);
+    }
+
+}
---
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/fft/FFT_transform_x.java
@@ -0,0 +1,4 @@
+package org.codehaus.groovy.gjit.soot.fft;
+
+public class FFT_transform_x extends sun.reflect.GroovyAOPMagic {
+}
