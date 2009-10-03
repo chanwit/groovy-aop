fded39d6f1b5c9794abfc2f134bc4b4b7b0cc16f
Chanwit Kaewkasi <chanwit@gmail.com>
2009-10-03 16:24:39 +0100

    added FFT opt test and callsite object

---
+++ src/test/groovy/org/codehaus/groovy/gjit/asm/FFTOptimisationTests.groovy
@@ -0,0 +1,47 @@
+package org.codehaus.groovy.gjit.asm
+
+import org.objectweb.asm.*
+import org.objectweb.asm.tree.*
+import org.objectweb.asm.util.*
+
+import groovy.util.GroovyTestCase
+import org.codehaus.groovy.gjit.soot.fft.*
+import org.codehaus.groovy.gjit.asm.transformer.*
+
+public class FFTOptimisationTests extends GroovyTestCase implements Opcodes {
+
+    static HEAPSORT_X = "org/codehaus/groovy/gjit/soot/heapsort/HeapSort_heapsort_x"
+
+    void testOptimiseOnHeapSort() {
+        InsnListHelper.install()
+
+        def sender = HeapSort.class
+        def sco  = new AsmSingleClassOptimizer()
+        def aatf = new AspectAwareTransformer(
+            advisedTypes:[int, double[]] as Class[],
+            advisedReturnType: null,
+            callSite: new FFT$transform(),
+            withInMethodName: "main"
+        )
+        sco.transformers = [
+          DeConstantTransformer.class,
+          aatf,
+          AutoBoxEliminatorTransformer.class
+        ]
+        byte[] bytes = sco.optimize(sender.name)
+        def cr = new ClassReader(bytes)
+        def cn = new ClassNode()
+        cr.accept cn, 0
+        def main = cn.methods.find { it.name == "main" }
+        assert main.name == "main"
+        // CheckClassAdapter.verify(cr, false, new PrintWriter(System.out))
+
+        def heapsort_x_body = ClassBodyCache.v().get(HEAPSORT_X)
+        assert heapsort_x_body != null
+        cr = new ClassReader(heapsort_x_body)
+        CheckClassAdapter.verify(cr, false, new PrintWriter(System.out))
+        def tcv = new TraceClassVisitor(new PrintWriter(System.out))
+        cr.accept tcv, 0
+    }
+
+}
---
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/fft/FFT$transform.java
@@ -0,0 +1,34 @@
+package org.codehaus.groovy.gjit.soot.fft;
+
+import org.codehaus.groovy.runtime.callsite.AbstractCallSite;
+import org.codehaus.groovy.runtime.callsite.CallSiteArray;
+
+class FFT$transform extends AbstractCallSite {
+
+    private static final String[] NAMES = new String[] {
+        "plus", "multiply", "minus", "multiply", "log2", "multiply", "plus", "transform_internal", "transform_internal", "length",
+        "div", "div", "putAt", "multiply", "getAt", "next", "length", "arraycopy", "transform", "inverse",
+        "iterator", "minus", "getAt", "getAt", "plus", "multiply", "sqrt", "div", "multiply", "<$constructor$>",
+        "iterator", "putAt", "nextDouble", "length", "println", "test", "makeRandom", "length", "parseInt", "getAt",
+        "println", "test", "makeRandom", "next", "next", "multiply", "leftShift", "<$constructor$>", "length", "div",
+        "length", "log2", "bitreverse", "div", "multiply", "multiply", "PI", "multiply", "sin", "sin",
+        "div", "multiply", "multiply", "multiply", "multiply", "plus", "getAt", "getAt", "plus", "putAt",
+        "minus", "getAt", "putAt", "plus", "minus", "getAt", "plus", "putAt", "plus", "getAt",
+        "putAt", "plus", "getAt", "plus", "plus", "multiply", "iterator", "minus", "minus", "multiply",
+        "multiply", "minus", "plus", "multiply", "multiply", "multiply", "plus", "multiply", "plus", "plus",
+        "getAt", "getAt", "plus", "minus", "multiply", "multiply", "plus", "multiply", "multiply", "putAt",
+        "minus", "getAt", "putAt", "plus", "minus", "getAt", "plus", "putAt", "plus", "getAt",
+        "putAt", "plus", "getAt", "plus", "plus", "multiply", "multiply", "next", "div", "length",
+        "minus", "multiply", "multiply", "div", "getAt", "getAt", "plus", "putAt", "getAt", "putAt",
+        "plus", "getAt", "plus", "putAt", "putAt", "plus", "minus", "div", "plus", "next"
+    };
+
+    public FFT$transform() {
+        super(new CallSiteArray(FFT.class, NAMES), 18, NAMES[18]);
+    }
+
+    public FFT$transform(int index) {
+        super(new CallSiteArray(FFT.class, NAMES), index, NAMES[index]);
+    }
+
+}
