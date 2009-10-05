bcd235f3fd17098859097d6ec4dc161567dedbae
Chanwit Kaewkasi <chanwit@gmail.com>
2009-10-06 00:07:11 +0100

    fixed type conversion for sieve

--- src/test/groovy/org/codehaus/groovy/gjit/asm/sieve/Caller.groovy
+++ src/test/groovy/org/codehaus/groovy/gjit/asm/sieve/Caller.groovy
@@ -4,7 +4,7 @@ class Caller {
 
     static realTest(n) {
         def start = System.nanoTime()
-        Sieve.sieve(n, 1)
+        Sieve.sieve(n, 1000)
         def stop = System.nanoTime()
         println "Sieve($n), time: ${stop-start}"
     }
