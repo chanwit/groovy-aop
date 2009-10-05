3fb9e14db9ec28d065f6c7fbe533488247163925
Chanwit Kaewkasi <chanwit@gmail.com>
2009-10-05 20:22:48 +0100

    updated benchmarks

--- src/test/groovy/org/codehaus/groovy/gjit/soot/fannkuch/Fannkuch$fannkuch.java
+++ src/test/groovy/org/codehaus/groovy/gjit/soot/fannkuch/Fannkuch$fannkuch.java
@@ -1,5 +1,21 @@
 package org.codehaus.groovy.gjit.soot.fannkuch;
 
-public class Fannkuch$fannkuch {
+import org.codehaus.groovy.runtime.callsite.AbstractCallSite;
+import org.codehaus.groovy.runtime.callsite.CallSiteArray;
 
+public class Fannkuch$fannkuch extends AbstractCallSite {
+
+	private static final String[] NAMES = new String[]{
+		"fannkuch","fannkuch","fannkuch",
+		"fannkuch","fannkuch","fannkuch",
+		"fannkuch","fannkuch","fannkuch",
+	};
+
+    public Fannkuch$fannkuch() {
+        super(new CallSiteArray(Fannkuch.class, NAMES), 9, "fannkuch");
+    }
+
+    public Fannkuch$fannkuch(int index) {
+        super(new CallSiteArray(Fannkuch.class, NAMES), index, NAMES[index]);
+    }
 }
