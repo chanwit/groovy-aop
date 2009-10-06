1bd4898eb9385aa6ceb95eccee68e163e3898a8c
Chanwit Kaewkasi <chanwit@gmail.com>
2009-10-06 01:20:51 +0100

    trying to fix if for fannkuch

--- src/test/groovy/org/codehaus/groovy/gjit/asm/fannkuch/FannkuchAspect.groovy
+++ src/test/groovy/org/codehaus/groovy/gjit/asm/fannkuch/FannkuchAspect.groovy
@@ -5,7 +5,7 @@ class FannkuchAspect {
         def pc = pcall('org.codehaus.groovy.gjit.asm.fannkuch.Fannkuch.fannkuch') & args(n)
         typing(pc) {
             n >> int
-            // return int
+            return int
         }
     }
 }
