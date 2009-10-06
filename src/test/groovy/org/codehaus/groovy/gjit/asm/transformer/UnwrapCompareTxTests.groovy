230c077457cf360e8a1ff489ecdabe87e2295685
chanwit <chanwit@gmail.com>
2009-10-07 01:04:18 +0700

    added compareEqual test case

--- src/test/groovy/org/codehaus/groovy/gjit/asm/transformer/UnwrapCompareTxTests.groovy
+++ src/test/groovy/org/codehaus/groovy/gjit/asm/transformer/UnwrapCompareTxTests.groovy
@@ -53,4 +53,33 @@ public class UnwrapCompareTxTests extends GroovyTestCase implements Opcodes {
         assert units[8].opcode == IF_ICMPGE
     }
 
+    void testUnwrap_CompareEqual_ForInt() {
+        InsnListHelper.install()
+
+        def mn = new MethodNode();
+        def units = mn.instructions
+        units.append {
+            iload 0
+            invokestatic Integer,"valueOf",[int],Integer
+            ldc 0
+            invokestatic Integer,"valueOf",[int],Integer
+            invokestatic SBA,"compareEqual",[Object,Object],boolean
+            def label_001 = new LabelNode()
+            ifeq label_001
+        }
+        def oldSize = units.size()
+        new UnwrapCompareTransformer().internalTransform(mn, null)
+        assert units.size() == oldSize + 2 + 2 - 1
+        assert asm {
+            iload 0
+            invokestatic  Integer,"valueOf", [int],Integer
+            checkcast Integer
+            invokevirtual Integer,"intValue",[],int
+            ldc 0
+            invokestatic  Integer,"valueOf", [int],Integer
+            checkcast Integer
+            invokevirtual Integer,"intValue",[],int
+        } == units[0..7]
+        assert units[8].opcode == IF_ICMPNE
+    }
 }
