777dff8c3531cd4a10ae6c1c960a027a403fd7fd
chanwit <chanwit@gmail.com>
2009-10-07 15:12:55 +0700

    updated script

--- benchmark_win.sh
+++ benchmark_win.sh
@@ -1,3 +1,4 @@
+export JAVA_HOME="c:\jdk"
 # Groovy GT
 $JAVA_HOME/bin/java -javaagent:./groovy-aop-0.6.1.jar -cp "groovy-aop-0.6.1.jar;*" org.codehaus.groovy.gjit.asm.heapsort.HeapSortTest  > gt_c_w_he.log
 $JAVA_HOME/bin/java -javaagent:./groovy-aop-0.6.1.jar -cp "groovy-aop-0.6.1.jar;*" org.codehaus.groovy.gjit.asm.fibbonacci.FibTest     > gt_c_w_fi.log
